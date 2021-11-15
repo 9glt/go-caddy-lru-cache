2bde8f51f319d9b9dbec1b073de1c73054a4354e
Mindaugas Žvirblis <mz@9g.lt>
2021-11-15 20:28:11 +0200

    testing things

--- lrucache.go
+++ lrucache.go
@@ -13,7 +13,6 @@ import (
 	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
 	"github.com/caddyserver/caddy/v2/caddyconfig/httpcaddyfile"
 	"github.com/caddyserver/caddy/v2/modules/caddyhttp"
-	"github.com/pkg/errors"
 
 	lrucache "github.com/9glt/go-caddy-lru-cache/golang-lru"
 	simplelru "github.com/9glt/go-caddy-lru-cache/golang-lru/simplelru"
@@ -97,11 +96,7 @@ func (rw RW) Write(b []byte) (int, error) {
 func (m Middleware) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddyhttp.Handler) error {
 	var err error
 	var value interface{}
-	log.Printf("%s", m.Output)
-	log.Printf("%s", r.URL.Path)
-	log.Printf("%s", r.RequestURI)
 	if strings.HasSuffix(r.URL.Path, m.Output) {
-		log.Printf(" ============== HAS SUFFIX")
 		value, err, _ = sf.Do(r.URL.Path, func() (interface{}, error) {
 			var value interface{}
 			var ok bool
@@ -116,13 +111,11 @@ func (m Middleware) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddy
 			}
 			err := next.ServeHTTP(buff, r)
 			log.Printf("mh: %v", cache.Add(r.URL.Path, buff.Bytes.Bytes()))
-			if err == nil {
-				err = errors.New("dont print")
-			}
 			return buff.Bytes.Bytes(), err
 		})
 		// if err == nil {
 		w.WriteHeader(200)
+		w.Header().Set("Content-Type", "text/vnd.trolltech.linguist")
 		w.Write(value.([]byte))
 		// }
 	} else {
